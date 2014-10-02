6d3c14bfa0e8383215bd68e81b502036dbefa544
Suraj Anandalwar <sda37@cornell.edu>
2014-10-02 00:43:57 -0400

    update

--- city_search_test.ml
+++ city_search_test.ml
@@ -1,7 +1,16 @@
 open City_search
 open Assertions
 
-TEST_UNIT "load_city_data" = assert_true (is_mon_inc [1;2;3])
+let ithaca = load_city_data "ithaca.csv"
+
+TEST_UNIT "city_search_test1" = assert_true( city_search ithaca ((42.44,-76.5),(42.5,-76.4)) = ["First Presbyterian Church"; "Fall Creek School"] )
+TEST_UNIT "city_search_test2" = assert_true( city_search ithaca ((42.43,-76.55),(42.5,-76.48)) = 
+	["Oldport Harbour"; "Greater Ithaca Activities Center"; "First Presbyterian Church"; "0alvary Baptist Church"; "Fall Creek School";    
+ "South Hill School"])
+(*Checking a region with a diagonal less than the min_diagonal constant*)
+TEST_UNIT "city_search_test3" = assert_true( city_search ithaca (42.43673949,-76.47660641),(42.43673951,-76.47660639))) = ["Belle Sherman School"] )
+
+
 
 
 
--- nats.ml
+++ nats.ml
@@ -50,8 +50,8 @@ let sign_int ( n : int ) : sign =
 let sum_overflows ( i1 : int ) ( i2 : int ) : bool =
   sign_int i1 = sign_int i2 && sign_int ( i1 + i2 ) <> sign_int i1
 let mult_overflows (i1:int) (i2:int) : bool =
-  if sign_int i1 = sign_int i2 then sign_int (i1*i2) <> Postive || i1*i2 < max i1 i2 else
-    sign_int (i1*i2) <> Negative ||  i1*i2 > (~-) max i1 i2
+  if sign_int i1 = sign_int i2 then sign_int (i1*i2) <> Positive || i1*i2 < max i1 i2 else
+    sign_int (i1*i2) <> Negative ||  i1*i2 > (~-) (max i1 i2)
 
 module IntNat : NATN = struct
   type t = int
@@ -59,11 +59,12 @@ module IntNat : NATN = struct
   let zero = 0
   let one = 1
   let ( + ) a b = if sum_overflows a b then raise Unrepresentable else Pervasives.(+) a b
-  let ( * ) a b = if mult_overflows a b then raise Unrepresentable else Pervasives ( * ) a b
+  let ( * ) a b = if mult_overflows a b then raise Unrepresentable else Pervasives.( * ) a b
   let ( < ) a b = a < b
   let ( === ) a b = a = b
   let int_of_nat a = a
   let nat_of_int a = a
+end 
 
 
 (* This helper function checks overflow. Returns boolean if there are max_int elements
@@ -84,18 +85,22 @@ The values of the list elements are irrelevant. *)
 	exception Unrepresentable
 	let zero = []
 	let one = [1]
-	let (+) a b = let x = List.fold_left (fun acc x -> x::acc) a b in if check_overflow x then raise Unrepresentable else x
-	let ( * ) a b = let z = List.fold_left (fun acc x -> List.rev_append a acc) [] b in if check_overflow z  then raise Unrepresentable
-						else z
-	let ( < ) a b =  let length x = List.fold_left (fun acc x -> acc + 1) 0 x in (length a) < (length b)
-	let ( === ) a b = let length x = List.fold_left (fun acc x -> acc + 1) 0 x in (length a) = (length b)
-	let int_of_nat a = let length x = List.fold_left (fun acc b -> acc +1) 0 x in if check_overflow a 
-							then raise Unrepresentable else length a
+	let (+) a b = let plus = List.fold_left (fun acc x -> x::acc) a b 
+                in if check_overflow plus then raise Unrepresentable else plus
+	let ( * ) a b = let mult = List.fold_left (fun acc x -> List.rev_append a acc) [] b  
+                  in if check_overflow mult then raise Unrepresentable else mult
+	let ( < ) a b =  let length l = List.fold_left (fun acc x -> Pervasives.(+) acc 1) 0 l 
+                   in (length a) < (length b)
+	let ( === ) a b = let length l = List.fold_left (fun acc x -> Pervasives.(+) acc 1) 0 l 
+                    in (length a) = (length b)
+	let int_of_nat a = let l = List.fold_left (fun acc x -> Pervasives.(+) acc 1) 0 a
+                     in if check_overflow a then raise Unrepresentable else l
 	let nat_of_int a = let rec concat (f: int list -> int list) (acc: int list) (x : int) : int list = 
-		match x with 
-		0 -> acc
-		| _ -> f (concat f acc (x-1))
-		in concat (fun acc -> 1::acc) [] a
+		                    match x with 
+		                    | 0 -> acc
+		                    | _ -> concat f (f acc) (x-1)
+		                 in concat (fun acc -> 1::acc) [] a
+end
 
 
 
@@ -104,10 +109,66 @@ The values of the list elements are irrelevant. *)
 to a natrual number. Here, there is no previous knowledge of the 
 representation type t. *)
 module NatConversionFn (N: NATN) = struct
-	let int_of_nat(n: N.t) : int = 
+	let int_of_nat (n: N.t) : int = N.int_of_nat n
+  let nat_of_int (n: int) : N.t = N.nat_of_int n
+end
+
+
+
+
 
 (* binary representation of naturals ex 10011 is = 19 because
 2^4 + 0 + 0 + 2^1 + 2^0 = 19. In a nested way, 10011 with base 2
 (((1*2+0)*2+0)*2+1)*2+1 = 19
-(for n bit number, (b(n-1) * 2 + b(n-2)*2 + b(n-3)*2..)*2+b(0)
+(for n bit number, (b(n-1) * 2 + b(n-2)*2 + b(n-3)*2..)*2+b(0)*)
 
+
+
+
+
+
+
+
+module AlienNatFn ( M : AlienMapping ): NATN with type t = M.aliensym list = struct
+  type t = M.aliensym list
+  let zero = [M.zero]
+  let one = [M.one]
+  let ( + ) a b = List.rev_append a b 
+  let ( * ) a b = let rec mult_aliensym (f: M.aliensym list -> M.aliensym -> M.aliensym list ) (acc: M.aliensym list) (i:int) (sym: M.aliensym) : M.aliensym list =
+                    match i with 
+                    | 0 -> M.zero::acc
+                    | _ -> mult_aliensym f (f acc sym) (i-1) sym
+                  in
+                  let mult_aliensym_list (c: M.aliensym) (d: M.aliensym list) : M.aliensym list =
+                    List.fold_left (fun acc x -> List.rev_append (mult_aliensym (fun acc1 x1 -> x1::acc1) [] (M.int_of_aliensym c) x ) acc ) [] d
+                  in 
+                  List.fold_left (fun acc x -> List.rev_append acc (mult_aliensym_list x b) ) [] a 
+
+  let less_than_helper (lst : M.aliensym list) : M.aliensym list =
+    List.fold_left (fun acc x -> let rec helper (f: t -> t) (acc: t) (i : int) : t =
+                                   match i with
+                                   | 0 -> acc
+                                   | _ -> helper f (f acc) (i-1)
+                                 in List.rev_append (helper (fun acc1 -> M.one::acc1) [] (M.int_of_aliensym x) ) acc ) [] lst
+
+
+  let ( < ) a b = let less_than = List.fold_left (fun acc x -> match acc with
+                                                               | (false,[]) -> (true,[]) 
+                                                               | (false, h::t) -> (false,t)
+                                                               | (true, _ ) -> (true,[])
+                                                                                            ) (false,less_than_helper a) (less_than_helper b) 
+                  in fst less_than
+  
+
+  let ( === ) a b = (less_than_helper a) = (less_than_helper b)
+
+  exception Unrepresentable
+
+  let int_of_nat a = List.fold_left (fun acc x -> if sum_overflows (M.int_of_aliensym x) acc then raise Unrepresentable else Pervasives.( + ) (M.int_of_aliensym x) acc) 0 a
+
+  let nat_of_int a = let rec helper (f: t -> t) (acc: t) (i: int) : t =
+                       match i with
+                       | 0 -> M.zero::acc
+                       | _ -> helper f (f acc) (i-1)
+                     in helper (fun b -> M.one::b) [] a
+end
